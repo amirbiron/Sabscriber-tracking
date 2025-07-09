ca9d6e0649db92e7ce5d3c623655393a5e8e105e
amir haim <amirbiron@gmail.com>
2025-07-09 03:29:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -47,4 +47,8 @@ async def main():
         raise
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import asyncio
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
